70f96f294890f101f131a6ca9176b56b02e8bb63
Arina Ageeva <arina.a.ageeva@gmail.com>
2019-04-19 00:30:25 +0300

    data for toloka

--- src/label_candidates_extract.py
+++ src/label_candidates_extract.py
@@ -11,23 +11,28 @@ class LabelCandidatesExtracter:
     This class is designed to extract label candidates from a text corpus.
     '''
     
-    def __init__(self, path=None, max_len=float('inf')):
+    def __init__(self, path=None, min_count=1, max_len=10):
         
         '''
         Parameters
-        ---------
+        ----------
         
-        path : str
+        path : str (default None)
             Path to the folder in which 
             the model parameters are dumped.
             
-        min_count : int
+        min_count : int (default 1)
             Candidates that are less common than this threshold 
+            are not extracted from the corps.
+            
+        max_len : int (default 10)
+            Candidates that have lenght less than this threshold 
             are not extracted from the corps.
         '''
         
         if path is None:
             
+            self._min_count = min_count
             self._max_len = max_len
             
             '''
@@ -36,8 +41,8 @@ class LabelCandidatesExtracter:
             self.total_tokens = 0
             
             '''
-            dict : Dictionary of sequences of tokens and their frequencies 
-                   (contain sequences that occur more than `min_count` times)
+            Counter : Counter of sequences of tokens and their frequencies 
+                     (contain sequences that occur more than `min_count` times).
             '''
             self.vocabulary_count = Counter()
             
@@ -47,19 +52,75 @@ class LabelCandidatesExtracter:
             
     def _init_boundaries(self, tokens):
         
+        '''
+        Parameters
+        ----------
+        tokens : list with namedtuple Token (initial in src.encoder).
+        
+        Return
+        ------
+        list with tuple : Sequence of tuple where each tuple contain 
+                          begin candidate index and end candidate index.
+                          Initially label candidates is tokens.
+        '''
+        
         N = len(tokens)
         return list(zip(range(N), range(1, N+1)))
     
     def _get_lemma(self, tokens):
+        
+        '''
+        Parameters
+        ----------
+        tokens : list with namedtuple Token (initial in src.encoder).
+        
+        Return
+        ------
+        str : Phrase where each token in lemma form.
+        '''
+        
         return ''.join([token.lemma+' '*token.space for token in tokens]).strip()
+    
+    def _crop_vocabulary_count(self, vocabulary_count):
+        
+        '''
+        Parameters
+        ----------
+        vocabulary_count : Counter where 
+                           key - sequences of tokens in lemma form 
+                           and values - frequency.
+        Return
+        ------
+        Counter : Counter of sequences of tokens and their frequencies 
+                  (contain sequences that occur more than `min_count` times).
+        '''
+        
+        return Counter(dict(filter(lambda x: x[1] >= self._min_count, vocabulary_count.items())))
     
     def _sig(self, tokens, separation_index):
         
+        '''
+        Parameters
+        ----------
+        tokens : list with namedtuple Token (initial in src.encoder).
+        
+        separation_index : int (from 1 to number of tokens minus 1)
+        
+        Return
+        ------
+        float : Significance from TopMine or 
+                -inf if candidate isn't in `vocabulary_count`.
+        '''
+        
         candidate = self._get_lemma(tokens)
+        candidate_count = self.vocabulary_count[candidate]
+        
+        if not candidate_count:
+            return -float('inf')
+        
         left_part = self._get_lemma(tokens[:separation_index])
         right_part = self._get_lemma(tokens[separation_index:])
             
-        candidate_count = self.vocabulary_count[candidate]     
         left_part_count = self.vocabulary_count[left_part]
         right_part_count = self.vocabulary_count[right_part]
             
@@ -75,7 +136,6 @@ class LabelCandidatesExtracter:
         
         Parameters
         ----------
-        
         sentences : list with Sent
         
         Return
@@ -86,13 +146,15 @@ class LabelCandidatesExtracter:
         sentence_tokens = [sentence.tokens for sentence in sentences]
         sentence_boundaries = [self._init_boundaries(tokens) for tokens in sentence_tokens]
         
-        self.vocabulary_count = Counter([token.lemma for token in chain(*sentence_tokens)])
-        self.total_tokens = sum(self.vocabulary_count.values())
+        tokens = [token.lemma for token in chain(*sentence_tokens)]
         
-        maximum_candidate_length = max([len(tokens) for tokens in sentence_tokens])
-        maximum_candidate_length = min(maximum_candidate_length, self._max_len)
+        self.total_tokens += len(tokens)
+        self.vocabulary_count += self._crop_vocabulary_count(Counter(tokens))
         
-        for _ in tqdm(range(maximum_candidate_length)):
+        max_candidate_len = max([len(tokens) for tokens in sentence_tokens])
+        max_candidate_len = min(max_candidate_len, self._max_len)
+        
+        for _ in tqdm(range(max_candidate_len)):
             
             if not sentence_boundaries:
                 break
@@ -121,8 +183,8 @@ class LabelCandidatesExtracter:
                     
             sentence_tokens = new_sentence_tokens
             sentence_boundaries = new_sentence_boundaries
-
-            self.vocabulary_count += Counter(chunks)
+            
+            self.vocabulary_count += self._crop_vocabulary_count(Counter(chunks))
                     
         return self
     
@@ -167,8 +229,7 @@ class LabelCandidatesExtracter:
                     max_sig = sig
                     max_boundary_index = index
             
-            if max_boundary_index < 0:
-                break
+            if max_sig < 0: break
             
             begin_boundary, _ = boundaries[max_boundary_index]
             _, end_boundary = boundaries.pop(max_boundary_index+1)
@@ -179,5 +240,26 @@ class LabelCandidatesExtracter:
             
         return candidates
     
+    def dump(self, path):
+        
+        with open(path, 'w', encoding='utf-8') as fl:
+            fl.write(f'{self._min_count}\n{self._max_len}\n{self.total_tokens}\n')
+            fl.write('\n'.join([f'{token}\t{count}' for token, count in self.vocabulary_count.items()]))
+
+    def load(self, path):
+        
+        with open(path, 'r', encoding='utf-8') as fl:
+            attributes = fl.read().split('\n')
+            
+        self._min_count = int(attributes[0])
+        self._max_len = int(attributes[1])
+        self.total_tokens = int(attributes[2])
+        
+        attributes = [token.split('\t') for token in attributes[3:]]
+        attributes = [(token, int(count)) for token, count in attributes]
+        
+        self.vocabulary_count = Counter(dict(attributes))
+        
+        return self
     
     
